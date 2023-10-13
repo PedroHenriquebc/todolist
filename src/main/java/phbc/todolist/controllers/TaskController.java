bc8910a001ecae94117cb1a6ea3ace3d5235204e
Pedro Henrique <pedrohbc96@gmail.com>
2023-10-13 18:47:51 -0300

    feat: Adiciona validação para datas de início e fim de uma Task

--- src/main/java/phbc/todolist/controllers/TaskController.java
+++ src/main/java/phbc/todolist/controllers/TaskController.java
@@ -2,6 +2,8 @@ package phbc.todolist.controllers;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import phbc.todolist.models.TaskModel;
 import phbc.todolist.repositories.TaskRepository;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @RestController
@@ -19,10 +22,17 @@ public class TaskController {
     private TaskRepository taskRepository;
 
     @PostMapping
-    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
         var idUser = request.getAttribute("idUser");
         taskModel.setIdUser((UUID) idUser);
+        var currentDate = LocalDateTime.now();
+        if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de início/término não pode ser uma data passada");
+        }
+        if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+            return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de início não pode ser depois que a data de término");
+        }
         var task = taskRepository.save(taskModel);
-        return task;
+        return ResponseEntity.status(HttpStatus.OK).body(task);
     }
 }
